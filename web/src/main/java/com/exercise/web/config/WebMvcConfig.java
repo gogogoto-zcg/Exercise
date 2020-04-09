adede7f813f89d7fc2e14be0c80817a6ae1f6156
zcg <gogogoto_dev@126.com>
2020-04-09 10:45:18 +0800

    springboot整合swagger2

--- web/src/main/java/com/exercise/web/config/WebMvcConfig.java
+++ web/src/main/java/com/exercise/web/config/WebMvcConfig.java
@@ -4,18 +4,34 @@ import com.exercise.web.interceptor.DemoInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Configuration
-public class MvnInterceptorConfig extends WebMvcConfigurationSupport {
+public class WebMvcConfig extends WebMvcConfigurationSupport {
 
     @Autowired
     private DemoInterceptor demoInterceptor;
 
+    /**
+     * 注册拦截器
+     * @param registry
+     */
     @Override
     protected void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(demoInterceptor)
                 .addPathPatterns("/demo/*")
                 .order(1);
     }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
 }
--- web/src/main/java/com/exercise/web/controller/DemoController.java
+++ web/src/main/java/com/exercise/web/controller/DemoController.java
@@ -2,7 +2,10 @@ package com.exercise.web.controller;
 
 import com.exercise.web.model.DemoModel;
 import com.exercise.web.service.DemoService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,13 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
  * @Author : zcg
  * @Date : 2020/4/8 0008 - 11:15
  */
+@Api("Demo")
 @RestController
 public class DemoController {
 
     @Autowired
     private DemoService demoService;
 
-    @RequestMapping("/demo")
+    @ApiOperation("demo 接口测试mvc")
+    @GetMapping("/demo")
     public DemoModel helloWorld(){
         DemoModel data = demoService.getData();
         return data;
